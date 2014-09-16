9f4868750f2f2afb5a6849dc96e9edb29b8d0f86
cris1133 <cris11133@gmail.com>
2014-09-15 20:28:11 -0400

    Delete parser.py

--- parser.py
+++
@@ -1,9 +0,0 @@
-#! /home/cristopher/Enthought/Canopy_64bit/User/bin/python
-def parseExpression(expression):
-	tokens = []
-	for character in range(len(expression)):
-		if expression[1] != '(':
-			break
-		if expression[-1] != ')':
-			break
-		if 
